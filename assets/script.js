c73f441c1c2693bb0653d1458f2cdfadb8104209
ermalwebdev <101928596+ermalwebdev@users.noreply.github.com>
2023-02-17 17:34:27 +0100

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -161,9 +161,19 @@ hamburgerOpenClose.addEventListener("click", function () {
 		hamburgerIcon[0].classList.remove("active");
 		hamburgerIcon[1].classList.add("active");
 		menuContainer.style.display = "block";
+                document.querySelector("html").style.position = "fixed";
+		document.querySelector("html").style.top = "0px";
+		document.querySelector("html").style.left = "0px";
+		document.querySelector("html").style.right = "0px";
+		document.querySelector("html").style.overflow = "hidden";
 	} else {
 		hamburgerIcon[1].classList.remove("active");
 		hamburgerIcon[0].classList.add("active");
 		menuContainer.style.display = "none";
+		 document.querySelector("html").style.position = "";
+		document.querySelector("html").style.top = "";
+		document.querySelector("html").style.left = "";
+		document.querySelector("html").style.right = "";
+		document.querySelector("html").style.overflow = "";
 	}
 });
